6e25581bb26656e121dbe46648b2e59b3b8dad49
Maleika Waigel <maleika1991@web.de>
2026-09-16 17:54:33 +0200

    Add smoother snowman melting stages

--- ascii_art.py
+++ ascii_art.py
@@ -1,29 +1,73 @@
 # Snowman ASCII Art stages
 STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
-      ___  
-     /___\\ 
-     """
- ]
+    # Stage 0: Full snowman
+    """
+     ___
+    /___\\
+    (o o)
+    ( : )
+    ( : )
+    """,
+
+    # Stage 1
+    """
+     ___
+    /___\\
+    (o o)
+    ( : )
+    ( ~ 
+    """,
+
+    # Stage 2
+    """
+     ___
+    /___\\
+    (o o)
+    ( : )
+      ~
+    """,
+
+    # Stage 3
+    """
+     ___
+    /___\\
+    (o o)
+    ( : )
+    """,
+
+    # Stage 4
+    """
+     ___
+    /___\\
+    (o o)
+    ( ~ 
+    """,
+
+    # Stage 5
+    """
+     ___
+    /___\\
+    (o o)
+      ~ 
+    """,
+
+    # Stage 6
+    """
+     ___
+    /___\\
+    (o o)
+    """,
+
+    # Stage 7
+    """
+     ___
+    /___\\
+    (o 
+    """,
+
+    # Stage 8: Snowman completely melted
+    """
+     ___
+    /___\\
+    """
+]
